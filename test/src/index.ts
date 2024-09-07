2bd28483a817ed71ace313a985b6a3974152fe77
nobu-sh <chat@nobu.sh>
2024-09-06 23:00:25 -0500

    remove test

--- test/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Serenity } from "@serenityjs/serenity";
-
-// Create a new Serenity instance
-const serenity = new Serenity();
-
-// Start the server
-serenity.start().catch(console.error);
