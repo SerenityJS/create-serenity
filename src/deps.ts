fd4d69953cd8b250303b46ad13155552ab05109f
Payton Kerby <paytonmk744@outlook.com>
2025-01-15 09:58:00 -0600

    chore: updated deps and templates

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,12 @@
 // These are dependencies we install based on the version of SerenityJS the user wants to use.
 export const VersionDependentDependencies = [
-	"@serenityjs/core",
-	"@serenityjs/logger",
-	"@serenityjs/nbt",
-	"@serenityjs/plugins",
-	"@serenityjs/raknet"
+  "@serenityjs/core",
+  "@serenityjs/logger",
+  "@serenityjs/nbt",
+  "@serenityjs/plugins",
+  "@serenityjs/raknet",
+  "@serenityjs/protocol",
+  "@serenityjs/binarystream",
 ];
 
 // These are deps we install latest of always.
