dd4902233b69078db88d2a3e21c492166bc17444
iEnis <enis-neimi@outlook.de>
2025-01-30 13:21:35 +0100

    fixed a bug that didn't allow for creating project folders and fixed spacing issue with the npm run message

--- src/plop.ts
+++ src/plop.ts
@@ -80,7 +80,7 @@ export async function copy(config: PlopCopyAction, api: PlopActionAPI) {
 		throw error;
 	}
 
-	if (await fs.exists(destinationDir) && fs.readdirSync(destinationDir).length !== 0) {
+	if (await fs.exists(destinationDir) && (await fs.readdir(destinationDir)).length !== 0) {
 		if (api.answers.overwrite) {
 			operation.write(
 				`📂 deleting '${path.relative(destinationCwd, destinationDir)}'`
